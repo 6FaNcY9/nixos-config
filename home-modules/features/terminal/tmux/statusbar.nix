4377371a1eb95843a418a4906fe88844506a9b30
6FaNcY9 <29282675+6FaNcY9@users.noreply.github.com>
2026-02-24 03:10:58 +0100

    feat(tmux): add NF icons to statusbar segments
    - status-left: session name prefixed with  (nf-fa-terminal)
    - status-right load: prefixed with  (nf-fa-microchip)
    - status-right host: prefixed with  (nf-fa-desktop)
    - status-right time: prefixed with  (nf-fa-clock_o)
    - active window format: prefixed with  (nf-fa-angle_double_right)

--- home-modules/features/terminal/tmux/statusbar.nix
+++ home-modules/features/terminal/tmux/statusbar.nix
@@ -10,18 +10,18 @@
     set -g message-style "fg=${palette.text},bg=${palette.bg}"
 
     # Left: session name (bold) + window indicator
-    set -g status-left "#[fg=${palette.accent},bold] #S #[default]#[fg=${palette.muted}]|#[default] "
+    set -g status-left "#[fg=${palette.accent},bold]  #S #[default]#[fg=${palette.muted}]|#[default] "
 
     # Right: battery + load + host + time
     # Battery: Show percentage and charging status
     # Load: Show 1-min load average
     # Host: Hostname in accent color
     # Time: Date and time in accent color
-    set -g status-right "#[fg=${palette.warn}]#{?#{==:#{battery_percentage},},, #{battery_percentage} #{battery_icon}}#[default] #[fg=${palette.muted}]|#[default] #[fg=${palette.accent2}]#{?#{==:#(cat /proc/loadavg | cut -d' ' -f1),},, #(cat /proc/loadavg | cut -d' ' -f1)}#[default] #[fg=${palette.muted}]|#[default] #[fg=${palette.accent2}]#H#[default] #[fg=${palette.muted}]|#[default] #[fg=${palette.accent}]%Y-%m-%d %H:%M#[default]"
+    set -g status-right "#[fg=${palette.warn}]#{?#{==:#{battery_percentage},},, #{battery_percentage} #{battery_icon}}#[default] #[fg=${palette.muted}]|#[default] #[fg=${palette.accent2}]#{?#{==:#(cat /proc/loadavg | cut -d' ' -f1),},,  #(cat /proc/loadavg | cut -d' ' -f1)}#[default] #[fg=${palette.muted}]|#[default] #[fg=${palette.accent2}] #H#[default] #[fg=${palette.muted}]|#[default] #[fg=${palette.accent}] %Y-%m-%d %H:%M#[default]"
 
     # Window status format
     set -g window-status-format "#[fg=${palette.muted}]#I:#W#F#[default]"
-    set -g window-status-current-format "#[fg=${palette.accent},bold]#I:#W#F#[default]"
+    set -g window-status-current-format "#[fg=${palette.accent},bold] #W#F#[default]"
     set -g window-status-separator " #[fg=${palette.muted}]│#[default] "
   '';
 }
