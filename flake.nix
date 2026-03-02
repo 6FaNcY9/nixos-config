0879ee84bc9100e0d3e01e27de90aafe7eb05a5d
6FaNcY9 <29282675+6FaNcY9@users.noreply.github.com>
2026-03-02 10:17:23 +0100

    fix(flake): remove redundant extra-substituters from nixConfig, already in nix.nix system settings

--- flake.nix
+++ flake.nix
@@ -16,8 +16,6 @@
 
   nixConfig = {
     extra-experimental-features = "nix-command flakes";
-    extra-substituters = "https://vino-nixos-config.cachix.org";
-    extra-trusted-public-keys = "vino-nixos-config.cachix.org-1:8LFVkzmO/+crLWO0Q3bqWOOamVjScT3v1/PCHPiTiUU=";
   };
 
   inputs = {
