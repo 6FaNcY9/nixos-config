2f02272f586c5fb3dcb15e5e4a7c7fa33a62c06a
6FaNcY9 <29282675+6FaNcY9@users.noreply.github.com>
2026-05-01 15:53:04 +0200

    fix: suppress i3 startup notification for copyq toggle (--no-startup-id)

--- home-modules/features/desktop/i3/keybindings.nix
+++ home-modules/features/desktop/i3/keybindings.nix
@@ -60,7 +60,7 @@ let
     "${mod}+Shift+m" = "scratchpad show";
 
     "${mod}+n" = "exec joplin-desktop";
-    "${mod}+Shift+v" = "exec copyq toggle";
+    "${mod}+Shift+v" = "exec --no-startup-id copyq toggle";
     "${mod}+Return" = "exec alacritty";
     "${mod}+Shift+w" = "exec qutebrowser";
     "${mod}+d" = "exec rofi -show drun";
